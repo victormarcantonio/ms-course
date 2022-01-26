1c777857e723778176fd9a104c1da7f3c4bdc790
victormarcantonio <victor_marcantonio@hotmail.com>
2022-01-26 15:01:28 -0300

    user, role many to many association

---
+++ hr-user/src/main/java/com/example/hruser/entities/Role.java
@@ -0,0 +1,53 @@
+package com.example.hruser.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Role implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @Id
+    private Long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
